875a3c88df4479e39e617cce119c157be80fea39
Ruben <rubenmrosario87@gmail.com>
2021-09-26 23:12:01 -0400

    photo assets (danny ma renato) 🤪

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Home = () => {
   return (
     <Layout>
-      <div>Home page</div>
+      <div>
+        Home page
+        <StaticImage src="../assets/renato_large.png" />
+        <StaticImage src="../assets/danny_large.png" />
+      </div>
     </Layout>
   )
 }
